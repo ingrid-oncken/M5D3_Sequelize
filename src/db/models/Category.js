d3927cf7ef88813ae83dc178c6a7cfa7497aecdb
Ingrid Oncken <ingrid.oncken@gmail.com>
2021-08-25 14:23:26 +0100

    CATEGORY

---
+++ src/db/models/Category.js
@@ -0,0 +1,41 @@
+import sequelize from "../conn"
+import s from "sequelize"
+const { DataTypes } = s
+
+const Product = sequelize.define("product", {
+  id: {
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  category: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.NUMBER,
+    allowNull: false,
+  },
+  brand: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  image: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+    validate: {
+      isUrl: true,
+      msg: "Invalid url",
+    },
+  },
+})
+
+export default product
